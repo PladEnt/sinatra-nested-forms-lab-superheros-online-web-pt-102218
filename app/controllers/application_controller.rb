7b9af5101d89231cf42a74ab60cee22a30a27209
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-21 14:15:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,16 @@ class App < Sinatra::Base
     @team_name = params[team][name]
     @motto = params[team][motto]
     
-    params["team"]["members"][0]["name"]
+    @heros_name = []
+    @heros_power = []
+    @heros_bio = []
     
-    
+    @members = params[:team][:members]
+    @members.each do |hero|
+      @heros_name << hero[:name]
+      @heros_power << hero[:power]
+      @heros_bio << hero[:bio]
+    end
     erb :team
   end
 end
